e654994dbc1a33dbed973a5140b645e7f0a5b980
Corentin Delorme <codelorme@gmail.com>
2021-09-10 11:23:42 +0200

    Update siteconfig/urls.py: Add static URLs for development environment

--- siteconfig/urls.py
+++ siteconfig/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
 	path('accounts/', include('django.contrib.auth.urls')),
 	path('admin/', admin.site.urls),
-	path('counter/', include('counters.urls')),
-]
+	path('counters/', include('counters.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

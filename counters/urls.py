2f102218650d072b6a4faaaba231ed5a9735a88d
Corentin Delorme <codelorme@gmail.com>
2021-09-17 21:55:49 +0200

    Update counters: Refactor views, use object views when appropriate

--- counters/urls.py
+++ counters/urls.py
@@ -3,8 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.list_counters),
-	path('<str:counter>/', views.view_counter),
-	path('<str:counter>/add/', views.increment_counter),
+	path('', views.CounterListView.as_view()),
+	path('<str:slug>/', views.CounterDetailView.as_view()),
+	path('<str:counter>/increment/', views.increment_counter),
+	path('<str:counter>/list/', views.RecordListView.as_view()),
 	path('<str:counter>/delete/', views.delete_counter),
+	path('<str:counter>/delete/<int:record>/', views.delete_record),
+	path('<str:counter>/edit/<int:pk>/', views.RecordUpdateView.as_view()),
 ]
--- counters/views.py
+++ counters/views.py
@@ -1,52 +1,96 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Counter, Record
+from django.utils.decorators import method_decorator
+from .models import Counter, Record, RecordForm
 
 import json
 
 
-@login_required
-def list_counters(request):
-	counters = []
-	all_counters = Counter.objects.filter(user=request.user)
-	for counter in all_counters:
-		entries = Record.objects.filter(counter=counter)
-		counters.append({ 'name': counter.name, 'value': len(entries) })
-	context = { 'counters': counters }
-	return render(request, 'counters/index.html', context)
+@method_decorator(login_required, name='dispatch')
+class CounterListView(ListView):
 
-@login_required
-def view_counter(request, counter):
-	counter, created = Counter.objects.get_or_create(user=request.user, name=counter)
-	entries = Record.objects.filter(counter=counter)
-	context = { 'counter': counter, 'total': len(entries), 'labels': [], 'data': []}
+	model = Counter
+	paginage_by = 20
 
-	dates = {}
-	for entry in entries:
-		date = entry.created.strftime('%Y-%m-%d')
-		if date not in dates:
-			dates[date] = 0
-		dates[date] += 1
+	def get_queryset(self):
+		queryset = Counter.objects.filter(user=self.request.user)
+		for counter in queryset:
+			records = Record.objects.filter(counter=counter)
+			counter.records = len(records)
+		return queryset
 
-	for day, value in dates.items():
-		context['data'].append(value)
-		context['labels'].append(day)
+@method_decorator(login_required, name='dispatch')
+class RecordListView(ListView):
 
-	context['data'] = json.dumps(context['data'])
-	context['labels'] = json.dumps(context['labels'])
+	model = Record
+	paginate_by = 20
 
-	print(context['data'])
-	print(context['labels'])
+	def get_queryset(self):
+		self.counter = get_object_or_404(Counter, user=self.request.user, name=self.kwargs['counter'])
+		return Record.objects.filter(counter=self.counter)
 
-	return render(request, 'counters/view.html', context)
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['counter'] = self.counter
+		context['total'] = len(self.object_list)
+		return context
+
+@method_decorator(login_required, name='dispatch')
+class RecordUpdateView(UpdateView):
+
+	model = Record
+	fields = [ 'created' ]
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		context['counter'] = get_object_or_404(Counter, user=self.request.user, pk=self.object.counter_id)
+		return context
+
+@method_decorator(login_required, name='dispatch')
+class CounterDetailView(DetailView):
+
+	model = Counter
+
+	def get_object(self):
+		return get_object_or_404(Counter, user=self.request.user, name=self.kwargs['slug'])
+
+	def get_context_data(self, **kwargs):
+		context = super().get_context_data(**kwargs)
+		records = Record.objects.filter(counter=self.object).order_by('created')
+
+		context['total'] = len(records)
+		context['labels'] = []
+		context['data'] = []
+
+		dates = {}
+		for record in records:
+			date = record.created.strftime('%Y-%m-%d')
+			if date not in dates:
+				dates[date] = 0
+			dates[date] += 1
+
+		for day, value in dates.items():
+			context['data'].append(value)
+			context['labels'].append(day)
+
+		context['data'] = json.dumps(context['data'])
+		context['labels'] = json.dumps(context['labels'])
+
+		return context
+
+
+# Counters API
 
 @login_required
 def increment_counter(request, counter):
 	counter, created = Counter.objects.get_or_create(user=request.user, name=counter)
 	Record(counter=counter).save()
-	entries = Record.objects.filter(counter=counter)
-	return HttpResponse(len(entries))
+	records = Record.objects.filter(counter=counter)
+	return HttpResponse(len(records))
 
 @login_required
 def delete_counter(request, counter):
@@ -56,6 +100,22 @@ def delete_counter(request, counter):
 		counter.delete()
 
 	except Counter.DoesNotExist:
-		pass
+		return HttpResponse('No such counter')
 
-	return HttpResponse()
+	return HttpResponse('OK')
+
+@login_required
+def delete_record(request, counter, record):
+
+	try:
+		counter = Counter.objects.get(user=request.user, name=counter)
+		record = Record.objects.get(counter=counter, pk=record)
+		record.delete()
+
+	except Counter.DoesNotExist:
+		return HttpResponse('No such counter')
+
+	except Record.DoesNotExist:
+		return HttpResponse('No such record')
+
+	return HttpResponse('OK')

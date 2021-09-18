6830d93ce30b74e5ad812cf406a29192e753cc4c
Corentin Delorme <codelorme@gmail.com>
2021-09-18 15:10:58 +0200

    Update siteconfig settings: Enable new TimezoneMiddleware from profiles app

--- siteconfig/settings.py
+++ siteconfig/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'profiles.middleware.TimezoneMiddleware',
 ]
 
 ROOT_URLCONF = 'siteconfig.urls'

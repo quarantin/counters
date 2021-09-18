c848fc8c40935c06da56883c150c3484570aa0cd
Corentin Delorme <codelorme@gmail.com>
2021-09-18 19:13:39 +0200

    Update profiles views: Add mixin for success message to ProfileUpdateView

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.edit import UpdateView
+from django.contrib.messages.views import SuccessMessageMixin
 
 from .models import Profile
 
-class ProfileUpdateView(UpdateView):
+class ProfileUpdateView(SuccessMessageMixin, UpdateView):
 	model = Profile
 	fields = [ 'timezone' ]
 	success_url = '/accounts/profile/'
+	success_message = "Your settings have been saved successfully"
 
 	def get_object(self):
 		obj, created = Profile.objects.get_or_create(user=self.request.user)

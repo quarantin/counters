84d6b3f69146a79e06f94c33a1f2e8c061f40a83
Corentin Delorme <codelorme@gmail.com>
2021-09-18 16:51:49 +0200

    Update profiles middleware: Fix crash for anonymous users

--- profiles/middleware.py
+++ profiles/middleware.py
@@ -2,15 +2,20 @@ from django.utils import timezone
 
 from .models import Profile
 
+import pytz
+
 
 class TimezoneMiddleware:
 	def __init__(self, get_response):
 		self.get_response = get_response
 
 	def __call__(self, request):
-		profile, created = Profile.objects.get_or_create(user=request.user)
-		if profile.timezone:
-			timezone.activate(profile.timezone)
-		else:
+
+		if request.user.is_anonymous:
 			timezone.deactivate()
+
+		else:
+			profile, created = Profile.objects.get_or_create(user=request.user)
+			timezone.activate(profile.timezone or pytz.timezone('Europe/London'))
+
 		return self.get_response(request)
